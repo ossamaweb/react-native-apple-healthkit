acd4461e3a9851974555580ffca87855255ea49a
Sam Benallouch <ossama.web@gmail.com>
2017-09-03 15:59:57 +0100

    initial release

---
+++ RCTAppleHealthKit/RCTAppleHealthKit.h
@@ -0,0 +1,23 @@
+//
+//  RCTAppleHealthKit.h
+//  RCTAppleHealthKit
+//
+//  Created by Greg Wilson on 2016-06-26.
+//  Copyright © 2016 Greg Wilson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <HealthKit/HealthKit.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTUtils.h>
+#import <React/RCTLog.h>
+
+@interface RCTAppleHealthKit : NSObject <RCTBridgeModule>
+
+@property (nonatomic) HKHealthStore *healthStore;
+
+- (void)isHealthKitAvailable:(RCTResponseSenderBlock)callback;
+- (void)initializeHealthKit:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
+- (void)getModuleInfo:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
+
+@end
---
+++ README.md
@@ -0,0 +1,6 @@
+# New Direction
+> *This package is undergoing a [new direction](https://github.com/GregWilson/react-native-apple-healthkit/pull/38).*
+> *<strong>Use at your own risk</strong>*
+
+## New Development 
+[rn-apple-healthkit](https://github.com/terrillo/rn-apple-healthkit)
